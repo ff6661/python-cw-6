5a3ea473e92bedbcc136daf360229cb99cf82c82
ff6661 <ftmmhmdysnbsmr@gmail.com>
2023-09-06 19:34:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
 # write your code here
+person = {
+    'name' : 'omar',
+    'age' : 28,
+    'hobbies' : ['reading', 'swimming', 'hiking', 'tennis']
+
+}
+print(person['name'])
+print(person['age'])
+person['age'] = 30
+person['country'] = 'algeria'
+print(len(person))
+person['hobbies'].append('coding')
+#print(person)
+def check_hobbies(person):
+    if len(person['hobbies']) > 3:
+        print('wow you are amazing')
+    else:
+        print(':)')
+check_hobbies(person)
+
